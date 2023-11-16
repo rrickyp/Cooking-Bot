472ed026c7de99bfaae6016d9e2dc54419b6d7f6
rrickyp <rickyrp@connect.hku.hk>
2023-11-16 16:50:11 +0800

    added the feature for getting the image from camera and gallery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "hku.hk.cs.cooking_bot"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "hku.hk.cs.cooking_bot"
@@ -39,9 +39,10 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("com.android.volley:volley:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
