a6fb7aaa212bcf8962cb6533f590b5ec47b0ff60
griz <mariagrizelda2002@gmail.com>
2023-11-16 17:08:59 +0800

    add recommended_recipe.xml

---
+++ app/src/main/java/hku/hk/cs/cooking_bot/RecommendedRecipeActivity.kt
@@ -0,0 +1,11 @@
+package hku.hk.cs.cooking_bot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecommendedRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recommended_recipe)
+    }
+}
