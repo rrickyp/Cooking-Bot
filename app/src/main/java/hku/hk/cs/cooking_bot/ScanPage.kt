f0f248096bb7a3eb2d3423777198e516889d8d0a
gvalencios <georgyvalencio77@gmail.com>
2023-11-18 14:59:18 +0800

    scan page

--- app/src/main/java/hku/hk/cs/cooking_bot/ScanPage.kt
+++ app/src/main/java/hku/hk/cs/cooking_bot/ScanPage.kt
@@ -9,6 +9,8 @@ import android.provider.MediaStore
 import android.util.Base64
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -17,20 +19,22 @@ import java.io.ByteArrayOutputStream
 import org.json.JSONArray
 import org.json.JSONObject
 
-class get_ingredients : AppCompatActivity() {
+class ScanPage : AppCompatActivity() {
     private var btn_camera: Button? = null
     private var btn_gallery: Button? = null
     private var btn_upload: Button? = null
-    private var bitmap:Bitmap? = null
+    private var bitmap: Bitmap? = null
     private var url:String? = "http://10.70.56.177:5000/recognize"
     private var ingredients = arrayListOf<String>()
+    private var imageView: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_ingredients)
+        setContentView(R.layout.scan_page)
         btn_camera = findViewById(R.id.btn_camera)
         btn_gallery = findViewById(R.id.btn_gallery)
         btn_upload = findViewById(R.id.upload)
+        imageView = findViewById(R.id.imageView)
         btn_camera!!.setOnClickListener {
             getCamera()
         }
@@ -40,40 +44,32 @@ class get_ingredients : AppCompatActivity() {
         btn_upload!!.setOnClickListener {
             uploadImage()
         }
+
     }
-    fun getGallery() {
+
+    private fun getCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 8)
+    }
+
+    private fun getGallery() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         intent.setAction(Intent.ACTION_GET_CONTENT)
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), 7)
+    }
 
-
-    }
-    fun getCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 8)
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 7 && resultCode == RESULT_OK && data != null) {
-            val path: Uri? = data.data
-            bitmap = MediaStore.Images.Media.getBitmap(contentResolver, path)
-        }
-        if (requestCode == 8 && resultCode == RESULT_OK && data != null) {
-            bitmap = data.extras!!.get("data") as Bitmap
-        }
-    }
-    fun uploadImage() {
+    private fun uploadImage() {
         val stringRequest = object : StringRequest(
             Request.Method.POST, url,
             Response.Listener<String> { response ->
-                    val jsonArray = JSONArray(response)
-                    // insert the name into the array
-                    println(jsonArray.length())
-                    for (i in 0 until jsonArray.length()){
-                        val jsonObj = jsonArray.getJSONObject(i)
-                        ingredients.add(jsonObj.get("name") as String)
-                    }
+                val jsonArray = JSONArray(response)
+                // insert the name into the array
+                println(jsonArray.length())
+                for (i in 0 until jsonArray.length()){
+                    val jsonObj = jsonArray.getJSONObject(i)
+                    ingredients.add(jsonObj.get("name") as String)
+                }
                 println(ingredients)
             },
             Response.ErrorListener { error ->
@@ -88,6 +84,26 @@ class get_ingredients : AppCompatActivity() {
         val requestQueue = Volley.newRequestQueue(this)
         requestQueue.add(stringRequest)
     }
+
+    private fun updateIngredientsTextView(ingredients: List<String>) {
+        val tvIngredients = findViewById<TextView>(R.id.tv_ingredients)
+        val ingredientsText = ingredients.joinToString("\n")
+        tvIngredients.text = ingredientsText
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 7 && resultCode == RESULT_OK && data != null) {
+            val path: Uri? = data.data
+            bitmap = MediaStore.Images.Media.getBitmap(contentResolver, path)
+        }
+        if (requestCode == 8 && resultCode == RESULT_OK && data != null) {
+            bitmap = data.extras!!.get("data") as Bitmap
+        }
+        // Set the bitmap to the ImageView
+        imageView?.setImageBitmap(bitmap)
+    }
+
     private fun imageToString(bitmap: Bitmap): String {
         val stream = ByteArrayOutputStream()
 
