b3d01cb46990732e1859800fb1b10b770bdc6052
Dave Matthew <davematthew2910@gmail.com>
2023-11-18 13:49:56 +0800

    link the cook now button to the new page

--- app/src/main/java/hku/hk/cs/cooking_bot/Recommendation.kt
+++ app/src/main/java/hku/hk/cs/cooking_bot/Recommendation.kt
@@ -2,10 +2,24 @@ package hku.hk.cs.cooking_bot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Recommendation : AppCompatActivity() {
+    private var cook_now_button: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recommendation)
+        cook_now_button = findViewById(R.id.button2)
+
+        cook_now_button!!.setOnClickListener {
+
+            setContentView(R.layout.recommended_recipe);
+
+        }
+
     }
+
+
+
 }
