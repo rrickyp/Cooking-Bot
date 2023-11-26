600978380a048e4f7f9bbe1b145ebe6000a104e5
griz <mariagrizelda2002@gmail.com>
2023-11-26 18:09:55 +0800

    login+register

---
+++ app/src/main/java/hku/hk/cs/cooking_bot/login.kt
@@ -0,0 +1,11 @@
+package hku.hk.cs.cooking_bot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/hku/hk/cs/cooking_bot/register.kt
@@ -0,0 +1,11 @@
+package hku.hk.cs.cooking_bot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
